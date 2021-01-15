376a5d4053d0ea1c9b3a64982c645dfc004b79b2
manas144 <74405330+manas144@users.noreply.github.com>
2021-01-15 21:00:19 +0530

    Add files via upload

---
+++ AngryBirdsStage2.5StudentActivity-master/slingshot.js
@@ -0,0 +1,18 @@
+class slingshot {
+constructor (bodyA,bodyB){
+ var options={
+    bodyA: bodyA,
+    bodyB: bodyB,
+    length:10,
+    stiffness:0.5
+ }
+this.chain=Matter.Constraint.create(options);
+World.add(world,this.chain)
+}
+display(){
+    var pointA=this.chain.bodyA.position
+    var pointB=this.chain.bodyB.position
+    line (pointA.x,pointA.y,pointB.x,pointB.y)
+}
+
+}
